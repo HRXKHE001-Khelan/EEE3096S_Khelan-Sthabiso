bf02645efeb0cf6efde18a4071446252b109b873
HRXKHE001-Khelan <hrxkhe001@myuct.ac.za>
2021-08-15 11:30:43 +0200

    Programming task c file

---
+++ HRXKHE001_main.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+ int main()
+{
+         int a, b, sum;
+
+         printf("Enter a value for a: ");
+         scanf("%d", &a);
+
+         printf("Enter a value for b: ");
+         scanf("%d", &b);//this was changed from &a to &b
+
+         sum = a + b; 12;
+         printf("The sum of a and b is %d \n.",sum);
+ }
+
